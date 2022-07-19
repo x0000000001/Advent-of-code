f48365d458c536b981b37cd9e855ad75d81ba987
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-07-19 17:17:38 +0200

    day finished

--- 2015/day25/src/lib.rs
+++ 2015/day25/src/lib.rs
@@ -1,8 +1,19 @@
-pub type InputType = Vec<String>;
+pub type InputType = (i64,i64);
 
-pub fn result_1(_input: InputType) -> i64
+pub fn result_1((x,y): InputType) -> i64
 {
-    0
+    let mut code = 20151125;
+    let factor = 252533;
+    let modulo = 33554393;
+
+    let n = ((x+y)*(x+y-1))/2 - x + 1;
+
+    for _ in 1..n {
+        code *= factor;
+        code %= modulo;
+    }
+
+    code
 }
 
 
--- 2015/day25/src/main.rs
+++ 2015/day25/src/main.rs
@@ -22,30 +22,11 @@ pub fn read_input(path: &str) -> InputType
     let contents= fs::read_to_string(path)
     .expect("Something went wrong reading the file");
 
-    let input:Vec<String> = contents.lines().into_iter().map(|line| line.trim().to_owned()).collect();
+    let mut input:Vec<String> = contents.lines().into_iter().map(|line| line.trim().to_owned()).collect();
 
-    input
-}
+    input[0] = input[0].replace(",", "");
+    input[0] = input[0].replace(".", "");
+    let w = input[0].split_whitespace().collect::<Vec<&str>>();
 
-
-#[allow(dead_code)]
-const TEST_INPUT_PATH: &str = "test_input.txt";
-
-#[cfg(test)]
-mod test 
-{
-    use super::*;
-
-    #[test]
-    fn test1()
-    {
-        assert_eq!(result_1(read_input(INPUT_PATH)), 0);
-    }
-
-    
-    #[test]
-    fn test2()
-    {
-        assert_eq!(result_2(read_input(INPUT_PATH)), 0);
-    }
+    (w[w.len()-3].parse().unwrap(), w.last().unwrap().parse().unwrap())
 }
