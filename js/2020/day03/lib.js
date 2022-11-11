668d01ec2b16404689f4cc45aede4f2e449a8dc9
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2022-11-11 19:41:25 +0100

    2020 03

--- js/2020/day03/lib.js
+++ js/2020/day03/lib.js
@@ -1,16 +1,40 @@
 import { benchmark, test } from "./main.js";
 
 export const format = (lines) => {
-  return lines;
+  return lines.map((line) => Array.from(line).map((c) => c == "#"));
+};
+
+let iterate = (state, input, xIter, yIter) => {
+  if (input[state.y][state.x % input[0].length]) {
+    state.count++;
+  }
+  state.x += xIter;
+  state.y += yIter;
+
+  return state.y < input.length;
+};
+
+let count_trees = (xIter, yIter, input) => {
+  let state = { x: 0, y: 0, count: 0 };
+  while (iterate(state, input, xIter, yIter)) {}
+  return state.count;
 };
 
 const f0 = (input) => {
-  return 0;
+  return count_trees(3, 1, input);
 };
 
 const f1 = (input) => {
-  return 0;
+  return [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ]
+    .map(([xIter, yIter]) => count_trees(xIter, yIter, input))
+    .reduce((a, b) => a * b);
 };
 
-test(f0, f1, 0, 0);
+test(f0, f1, 7, 336);
 benchmark(f0, f1);
