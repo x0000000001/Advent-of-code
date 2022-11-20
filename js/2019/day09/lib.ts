68c2ceab4fc5a268825f0675f7064d17ecbb1d0c
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2022-11-20 13:05:17 +0100

    save before bigint

--- js/2019/day09/lib.ts
+++ js/2019/day09/lib.ts
@@ -1,49 +1,77 @@
 import { benchmark, test } from "./main.js";
 
-export const format = (lines: string[]) => {
-  return lines;
+type InputType = { [index: number]: number };
+
+export const format = (lines: string[]): InputType => {
+  let instrs: InputType = {};
+  lines[0].split(",").forEach((c, i) => (instrs[i] = parseInt(c)));
+  return instrs;
 };
 
-const run_instrs1 = (instrs) => {
+/**
+ *
+ * @param instrs instrs to be executed
+ * @param inputs inputs to feed to the programm
+ * @param i beginning instruction index
+ * @param relative_base relative base as defined for type 2 values
+ * @returns index of last instruction (null if done), outputs, relative_base value
+ */
+const run_instrs = (
+  instrs: InputType,
+  inputs: number[],
+  i = 0,
+  relative_base = 0
+): [number, number[], number] => {
   let output = [];
-  let i = 0;
+  let input_index = 0;
   while (true) {
-    let cmd = instrs[i];
+    let cmd = instrs[i] | 0;
+    let cmd_string: string = cmd.toString();
     let parameters = [null, null, null];
     for (let j = 0; j < 3; j++) {
-      let b = 0;
-      if (cmd.toString().length > 2 + j) {
-        b = (cmd % Math.pow(10, j + 3)) - (cmd % Math.pow(10, j + 2));
+      let mode = 0;
+      if (cmd_string.length > 2 + j) {
+        mode = parseInt(cmd_string.charAt(cmd_string.length - (3 + j)));
       }
-      let val = instrs[i + 1 + j];
-      if (b === 0) {
-        parameters[j] = instrs[val];
-      } else {
-        parameters[j] = val;
+      let val = instrs[i + 1 + j] | 0;
+      switch (mode) {
+        case 0:
+          parameters[j] = instrs[val] | 0;
+          break;
+        case 1:
+          parameters[j] = val;
+          break;
+        case 2:
+          parameters[j] = instrs[relative_base + val] | 0;
+          break;
       }
     }
     cmd = cmd % 100;
     switch (cmd) {
       case 1:
-        instrs[instrs[i + 3]] = parameters[0] + parameters[1];
+        instrs[instrs[i + 3] | 0] = parameters[0] + parameters[1];
         i += 4;
         break;
 
       case 2:
-        instrs[instrs[i + 3]] = parameters[0] * parameters[1];
+        instrs[instrs[i + 3] | 0] = parameters[0] * parameters[1];
         i += 4;
         break;
 
       case 99:
-        return output;
+        return [null, output, relative_base];
 
       case 3:
-        instrs[instrs[i + 1]] = 5;
+        if (input_index == inputs.length) {
+          return [i, output, relative_base];
+        }
+        instrs[instrs[i + 1] | 0] = inputs[input_index];
+        input_index++;
         i += 2;
         break;
 
       case 4:
-        output += parameters[0];
+        output.push(parameters[0]);
         i += 2;
         break;
 
@@ -65,20 +93,25 @@ const run_instrs1 = (instrs) => {
 
       case 7:
         if (parameters[0] < parameters[1]) {
-          instrs[instrs[i + 3]] = 1;
+          instrs[instrs[i + 3] | 0] = 1;
         } else {
-          instrs[instrs[i + 3]] = 0;
+          instrs[instrs[i + 3] | 0] = 0;
         }
         i += 4;
         break;
 
       case 8:
         if (parameters[0] === parameters[1]) {
-          instrs[instrs[i + 3]] = 1;
+          instrs[instrs[i + 3] | 0] = 1;
         } else {
-          instrs[instrs[i + 3]] = 0;
+          instrs[instrs[i + 3] | 0] = 0;
         }
         i += 4;
+        break;
+
+      case 9:
+        relative_base += parameters[0];
+        i += 2;
         break;
 
       default:
@@ -87,8 +120,9 @@ const run_instrs1 = (instrs) => {
   }
 };
 
-const f0 = (input) => {
-  return 0;
+const f0 = (instrs) => {
+  let res = run_instrs(instrs, [1]);
+  return res[1];
 };
 
 const f1 = (input) => {
@@ -96,8 +130,9 @@ const f1 = (input) => {
 };
 
 test([
-  { f: f0, expected: 0 },
-  { f: f1, expected: 0 },
+  { f: f0, expected: 0, file: "test_input0.txt" },
+  { f: f0, expected: 0, file: "test_input1.txt" },
+  { f: f0, expected: 0, file: "test_input2.txt" },
 ]);
 
 benchmark(f0, f1);
