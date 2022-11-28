193db17b7efd19d51f4c3b9d6aee8a2aa09e117f
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-11-28 17:05:12 +0100

    transfer

--- go/2020/day22/lib.go
+++ go/2020/day22/lib.go
@@ -13,12 +13,18 @@ type intFIFO struct {
 }
 
 func (f *intFIFO) insert(x int) {
+	if f.c == len(f.els) {
+		panic("can't insert in full queue")
+	}
 	f.els[f.insert_ptr] = x
 	f.insert_ptr = (f.insert_ptr + 1) % len(f.els)
 	f.c++
 }
 
 func (f *intFIFO) pop() int {
+	if f.c == 0 {
+		panic("can't pop empty queue")
+	}
 	temp := f.els[f.pop_ptr]
 	f.pop_ptr = (f.pop_ptr + 1) % len(f.els)
 	f.c--
@@ -77,23 +83,14 @@ func format(lines []string) InputType {
 }
 
 func (p *intFIFO) score() int {
+	clone := p.clone()
 	s := 0
 
-	for p.count() != 0 {
-		s += p.count() * p.pop()
+	for clone.count() != 0 {
+		s += clone.count() * clone.pop()
 	}
 
 	return s
-}
-
-func (p *intFIFO) clone() intFIFO {
-	f := make_intFIFO(len(p.els))
-
-	for p.count() != 0 {
-		f.insert(p.pop())
-	}
-
-	return f
 }
 
 func part1(input InputType) RetType {
@@ -122,12 +119,13 @@ func part1(input InputType) RetType {
 }
 
 func equal_intFIFO(f0 intFIFO, f1 intFIFO) bool {
-	if f0.count() != f1.count() {
+	clone0, clone1 := f0.clone(), f1.clone()
+	if clone0.count() != clone1.count() {
 		return false
 	}
 
-	for f0.count() > 0 {
-		if f0.pop() != f0.pop() {
+	for clone0.count() > 0 {
+		if clone0.pop() != clone1.pop() {
 			return false
 		}
 	}
@@ -167,23 +165,60 @@ func (s *intFIFOSet) has(f intFIFO) bool {
 	}
 }
 
-func recursive_game(p0 intFIFO, p1 intFIFO, seen0 intFIFOSet, seen1 intFIFOSet) (int, intFIFO, intFIFO) {
-	if seen0.has(p0) {
-		return 0, p0, p1
-	} else if seen1.has(p1) {
-		return 1, p1, p1
+func (f *intFIFO) clone() intFIFO {
+	c := *f
+
+	c.els = make([]int, len(f.els))
+	copy(c.els, f.els)
+
+	return c
+}
+
+// winning id, score
+func recursive_game(p0 intFIFO, p1 intFIFO) (int, int) {
+	seen0, seen1 := make(intFIFOSet), make(intFIFOSet)
+
+	for !seen0.has(p0) && !seen1.has(p1) {
+
+		if p0.count() == 0 {
+			return 1, p1.score()
+		} else if p1.count() == 0 {
+			return 0, p0.score()
+		}
+
+		seen0.add(p0.clone())
+		seen1.add(p1.clone())
+
+		c0 := p0.pop()
+		c1 := p1.pop()
+
+		if c0 <= p0.count() && c1 <= p1.count() {
+			winner, _ := recursive_game(p0.clone(), p1.clone())
+			// p0, p1 = new_p0, new_p1
+			if winner == 0 {
+				p0.insert(c0)
+				p0.insert(c1)
+			} else {
+				p1.insert(c1)
+				p1.insert(c0)
+			}
+		} else {
+			if c0 > c1 {
+				p0.insert(c0)
+				p0.insert(c1)
+			} else {
+				p1.insert(c1)
+				p1.insert(c0)
+			}
+		}
 	}
 
-	c0 := p0.pop()
-	c1 := p1.pop()
-
-	if c0 <= p0.count() && c1 <= p1.count() {
-		winner, new_p0, new_p1 := recursive_game(p0.clone(), p1.clone(), make(intFIFOSet), make(intFIFOSet))
-	}
-
-	return -1, p0, p1
+	return 1, p1.score()
 }
 
 func part2(input InputType) RetType {
-	return 0
+	p0, p1 := input[0], input[1]
+	_, score := recursive_game(p0.clone(), p1.clone())
+
+	return score
 }
