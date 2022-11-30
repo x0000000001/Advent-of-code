08841b1019d568a36ce149c93f0a3d1b1bcd4885
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-11-30 22:17:10 +0100

    2019 12

---
+++ rust/2018/day12/lib.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+pub type InputType = Vec<String>;
+
+pub fn result_1(input: InputType) -> i64 {
+    0
+}
+
+pub fn result_2(input: InputType) -> i64 {
+    0
+}
+
+pub fn read_input(path: &str) -> InputType {
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+
+    let input: Vec<String> = contents
+        .lines()
+        .into_iter()
+        .map(|line| line.trim().to_owned())
+        .filter(|l| !l.is_empty())
+        .collect();
+
+    input
+}
