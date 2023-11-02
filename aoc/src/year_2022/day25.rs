3b29121ee2201679f0fae9c86e0c8a7bad58b9db
x0000000001 <antoine.toussaint2002@gmail.com>
2023-11-01 22:51:29 -0400

    moved 5 five of 2022

--- aoc/src/year_2022/day25.rs
+++ aoc/src/year_2022/day25.rs
@@ -1,19 +1,78 @@
 use crate::Solution;
 
-type InputType = Vec<char>;
+type InputType = Vec<SNAFU>;
+
+type SNAFU = String;
+
+fn snafu_to_i64(sn: &SNAFU) -> i64 {
+    let mut n = 0;
+
+    for c in sn.chars() {
+        n *= 5;
+        n += match c {
+            '2' => 2,
+            '1' => 1,
+            '0' => 0,
+            '-' => -1,
+            '=' => -2,
+            _ => panic!(),
+        }
+    }
+
+    n
+}
+
+fn i64_to_snafu(mut n: i64) -> SNAFU {
+    let mut current_power = 1;
+    while current_power * 5 <= n.abs() {
+        current_power *= 5;
+    }
+
+    let mut s = vec![];
+
+    while current_power > 0 {
+        let mut i = -4;
+
+        for k in -2..3 {
+            if (k * current_power - n).abs() < current_power / 2 + 1 {
+                i = k;
+                break;
+            }
+        }
+
+        n -= i * current_power;
+        s.push(i);
+        current_power /= 5;
+    }
+
+    s.into_iter()
+        .map(|i| match i {
+            2 => '2',
+            1 => '1',
+            0 => '0',
+            -1 => '-',
+            -2 => '=',
+            _ => panic!(),
+        })
+        .collect()
+}
 
 pub fn part1(s: String) -> Solution {
     let input = parse(s);
 
-    Solution::NotImplemented
+    Solution::from(i64_to_snafu(
+        input.into_iter().map(|sn| snafu_to_i64(&sn)).sum(),
+    ))
 }
 
-pub fn part2(s: String) -> Solution {
-    let input = parse(s);
-
-    Solution::NotImplemented
+pub fn part2(_: String) -> Solution {
+    Solution::Day25Part2
 }
 
 fn parse(s: String) -> InputType {
-    s.chars().collect()
+    s.lines()
+        .into_iter()
+        .map(|line| line.trim().to_owned())
+        .filter(|l| !l.is_empty())
+        .collect()
 }
