dbb515260fb4b33a26d5437f0d40cd7ef3644b8c
x0000000001 <antoine.toussaint2002@gmail.com>
2023-11-02 14:40:32 -0400

    tests setup for all years

--- aoc/templates/day_test.rs
+++ aoc/templates/day_test.rs
@@ -6,3 +6,10 @@ fn part1_test_input0() {
         .unwrap()
         .test_input(1, 0, aoc::Solution::from(3))
 }
+
+#[test]
+fn part2_test_input0() {
+    DAYS.get("2015_01")
+        .unwrap()
+        .test_input(1, 0, aoc::Solution::from(3))
+}
