a17d302442a0bd91461e8490e5a64a3b4eec36d6
x0000000001 <antoine.toussaint2002@gmail.com>
2023-12-13 17:32:23 -0500

    clean templates

---
+++ templates/daypy/day.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def f1(input):
+    pass
+
+
+def f2(input):
+    pass
+
+
+def parse(s):
+    return None
+
+
+with open(sys.argv[1]) as f1:
+    s = f1.read().strip()
+
+input = parse(s)
+f1(input)
+f2(input)
